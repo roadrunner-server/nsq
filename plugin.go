d1f5238a3bf6d89db16e7159a72b1e132b9caeda
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-06-22 00:45:54 +0200

    feature: NSQ Jobs driver
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ plugin.go
@@ -0,0 +1,66 @@
+package nsq
+
+import (
+	"github.com/roadrunner-server/api/v4/plugins/v3/jobs"
+	"github.com/roadrunner-server/endure/v2/dep"
+	"github.com/roadrunner-server/errors"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.uber.org/zap"
+)
+
+const pluginName string = "nsq"
+
+type Configurer interface {
+	// UnmarshalKey takes a single key and unmarshals it into a Struct.
+	UnmarshalKey(name string, out any) error
+	// Has checks if config section exists.
+	Has(name string) bool
+}
+
+type Logger interface {
+	NamedLogger(name string) *zap.Logger
+}
+
+type Tracer interface {
+	Tracer() *sdktrace.TracerProvider
+}
+
+type Plugin struct {
+	log    *zap.Logger
+	cfg    Configurer
+	tracer *sdktrace.TracerProvider
+}
+
+func (p *Plugin) Init(log Logger, cfg Configurer) error {
+	if !cfg.Has(pluginName) {
+		return errors.E(errors.Disabled)
+	}
+
+	p.log = log.NamedLogger(pluginName)
+	p.cfg = cfg
+	return nil
+}
+
+func (p *Plugin) Name() string {
+	return pluginName
+}
+
+func (p *Plugin) Collects() []*dep.In {
+	return []*dep.In{
+		dep.Fits(func(pp any) {
+			p.tracer = pp.(Tracer).Tracer()
+		}, (*Tracer)(nil)),
+	}
+}
+
+// DriverFromConfig constructs kafka driver from the .rr.yaml configuration
+func (p *Plugin) DriverFromConfig(configKey string, pq jobs.Queue, pipeline jobs.Pipeline, _ chan<- jobs.Commander) (jobs.Driver, error) {
+	return nil, nil
+	// return amqpjobs.FromConfig(p.tracer, configKey, p.log, p.cfg, pipeline, pq)
+}
+
+// DriverFromPipeline constructs kafka driver from pipeline
+func (p *Plugin) DriverFromPipeline(pipe jobs.Pipeline, pq jobs.Queue, _ chan<- jobs.Commander) (jobs.Driver, error) {
+	return nil, nil
+	// return amqpjobs.FromPipeline(p.tracer, pipe, p.log, p.cfg, pq)
+}
